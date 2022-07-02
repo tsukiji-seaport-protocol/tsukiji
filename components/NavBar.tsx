39b96cd35d8fdf4ac4a4e590d76a2f809a2763c9
iamminci <0xminci@gmail.com>
2022-07-02 09:34:12 -0400

    REFACTORED: sidebar from navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,21 +1,8 @@
-import {
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-  Button,
-  IconButton,
-  Link,
-  Image,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Link, Image, useDisclosure } from "@chakra-ui/react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styles from "@styles/NavBar.module.css";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { FaGithub } from "react-icons/fa";
+import { Sidebar } from "./Sidebar";
 
 export const NavBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -35,63 +22,7 @@ export const NavBar = () => {
           h={7}
         />
       </div>
-      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-        <DrawerOverlay />
-        <DrawerContent
-          bgColor="#000000"
-          borderLeft="1px solid rgba(255,255,255,.3)"
-          paddingTop="3rem"
-        >
-          <DrawerCloseButton color="#FFFFFF" />
-
-          <DrawerBody className={styles.drawerBody}>
-            <Button className={styles.drawerButton}>EXPLORE</Button>
-            <Button className={styles.drawerButton}>CREATE</Button>
-            <Button className={styles.drawerButton}>RANKINGS</Button>
-            <Button className={styles.drawerButton}>HOW IT WORKS</Button>
-            <Button className={styles.drawerButton}>BLOG</Button>
-          </DrawerBody>
-
-          <DrawerFooter className={styles.drawerFooter}>
-            <div className={styles.drawerFooterLabel}>Made With ❤️ By</div>
-            <Button className={styles.drawerFooterButton}>
-              <a
-                href="https://twitter.com/jeongminc_"
-                target="
-              _blank"
-              >
-                @jeongminc_
-              </a>
-            </Button>
-            <Button className={styles.drawerFooterButton}>
-              <a
-                href="https://twitter.com/straightupjac"
-                target="
-              _blank"
-              >
-                @straightupjac
-              </a>
-            </Button>
-            <Button className={styles.drawerFooterButton}>
-              <a
-                href="https://twitter.com/andrewkjmin"
-                target="
-              _blank"
-              >
-                @andrewkjmin
-              </a>
-            </Button>
-            <a href="https://github.com/tsukiji-seaport-protocol/tsukiji" rel="noreferrer" target="_blank" >
-        <IconButton
-          aria-label="github icon"
-          colorScheme="dark"
-          variant="ghost"
-          icon={<FaGithub />}
-        />
-      </a>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+      <Sidebar isOpen={isOpen} onClose={onClose} />
     </div>
   );
 };
---
+++ components/Sidebar.tsx
@@ -0,0 +1,86 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  IconButton,
+} from "@chakra-ui/react";
+import styles from "@styles/NavBar.module.css";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+
+type SidebarProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
+  return (
+    <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+      <DrawerOverlay />
+      <DrawerContent
+        bgColor="#000000"
+        borderLeft="1px solid rgba(255,255,255,.3)"
+        paddingTop="3rem"
+      >
+        <DrawerCloseButton color="#FFFFFF" />
+
+        <DrawerBody className={styles.drawerBody}>
+          <Button className={styles.drawerButton}>EXPLORE</Button>
+          <Link href="/create">
+            <Button className={styles.drawerButton}>CREATE</Button>
+          </Link>
+          <Button className={styles.drawerButton}>RANKINGS</Button>
+          <Button className={styles.drawerButton}>HOW IT WORKS</Button>
+          <Button className={styles.drawerButton}>BLOG</Button>
+        </DrawerBody>
+
+        <DrawerFooter className={styles.drawerFooter}>
+          <div className={styles.drawerFooterLabel}>Made With ❤️ By</div>
+          <Button className={styles.drawerFooterButton}>
+            <a
+              href="https://twitter.com/jeongminc_"
+              target="
+              _blank"
+            >
+              @jeongminc_
+            </a>
+          </Button>
+          <Button className={styles.drawerFooterButton}>
+            <a
+              href="https://twitter.com/straightupjac"
+              target="
+              _blank"
+            >
+              @straightupjac
+            </a>
+          </Button>
+          <Button className={styles.drawerFooterButton}>
+            <a
+              href="https://twitter.com/andrewkjmin"
+              target="
+              _blank"
+            >
+              @andrewkjmin
+            </a>
+          </Button>
+          <a
+            href="https://github.com/tsukiji-seaport-protocol/tsukiji"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <IconButton
+              aria-label="github icon"
+              colorScheme="dark"
+              variant="ghost"
+              icon={<FaGithub />}
+            />
+          </a>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};

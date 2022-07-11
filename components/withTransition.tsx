af6f157c1dcce3803ea3df5564490dda4f537c8f
iamminci <0xminci@gmail.com>
2022-07-11 10:03:31 -0400

    FIXED: flickering transition

--- components/withTransition.tsx
+++ components/withTransition.tsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 export default function withTransition<T>(Component: React.ComponentType<T>) {
   return (props: T) => (
     <motion.div
-      exit={{ opacity: 0 }}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.3, ease: "easeInOut" }}
